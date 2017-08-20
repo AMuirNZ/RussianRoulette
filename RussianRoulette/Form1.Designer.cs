83d31dc38fc8f467348fa35c2d5a8a0911ce93ba
Alisdair <Alisdair@LAPTOP-TKI3D179>
2017-08-21 10:03:26 +1200

    Add project files.

---
+++ RussianRoulette/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace RussianRoulette
+{
+    partial class RussianRoulette
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblWins = new System.Windows.Forms.Label();
+            this.lblLosses = new System.Windows.Forms.Label();
+            this.btnShoot = new System.Windows.Forms.Button();
+            this.btnPointAway = new System.Windows.Forms.Button();
+            this.btnSpin = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblRandom = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblBulletsFired = new System.Windows.Forms.Label();
+            this.lblAway = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 225);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Wins";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 225);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Losses";
+            // 
+            // lblWins
+            // 
+            this.lblWins.AutoSize = true;
+            this.lblWins.Location = new System.Drawing.Point(16, 259);
+            this.lblWins.Name = "lblWins";
+            this.lblWins.Size = new System.Drawing.Size(13, 13);
+            this.lblWins.TabIndex = 2;
+            this.lblWins.Text = "0";
+            // 
+            // lblLosses
+            // 
+            this.lblLosses.AutoSize = true;
+            this.lblLosses.Location = new System.Drawing.Point(60, 259);
+            this.lblLosses.Name = "lblLosses";
+            this.lblLosses.Size = new System.Drawing.Size(13, 13);
+            this.lblLosses.TabIndex = 3;
+            this.lblLosses.Text = "0";
+            // 
+            // btnShoot
+            // 
+            this.btnShoot.Enabled = false;
+            this.btnShoot.Location = new System.Drawing.Point(16, 64);
+            this.btnShoot.Name = "btnShoot";
+            this.btnShoot.Size = new System.Drawing.Size(75, 23);
+            this.btnShoot.TabIndex = 4;
+            this.btnShoot.Text = "Shoot";
+            this.btnShoot.UseVisualStyleBackColor = true;
+            this.btnShoot.Click += new System.EventHandler(this.btnShoot_Click);
+            // 
+            // btnPointAway
+            // 
+            this.btnPointAway.Enabled = false;
+            this.btnPointAway.Location = new System.Drawing.Point(16, 122);
+            this.btnPointAway.Name = "btnPointAway";
+            this.btnPointAway.Size = new System.Drawing.Size(75, 23);
+            this.btnPointAway.TabIndex = 5;
+            this.btnPointAway.Text = "Point Away";
+            this.btnPointAway.UseVisualStyleBackColor = true;
+            this.btnPointAway.Click += new System.EventHandler(this.btnPointAway_Click);
+            // 
+            // btnSpin
+            // 
+            this.btnSpin.Location = new System.Drawing.Point(16, 93);
+            this.btnSpin.Name = "btnSpin";
+            this.btnSpin.Size = new System.Drawing.Size(75, 23);
+            this.btnSpin.TabIndex = 6;
+            this.btnSpin.Text = "Spin Chamber";
+            this.btnSpin.UseVisualStyleBackColor = true;
+            this.btnSpin.Click += new System.EventHandler(this.btnSpin_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(289, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Random Number";
+            // 
+            // lblRandom
+            // 
+            this.lblRandom.AutoSize = true;
+            this.lblRandom.Location = new System.Drawing.Point(328, 259);
+            this.lblRandom.Name = "lblRandom";
+            this.lblRandom.Size = new System.Drawing.Size(0, 13);
+            this.lblRandom.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(216, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Bullets Fired";
+            // 
+            // lblBulletsFired
+            // 
+            this.lblBulletsFired.AutoSize = true;
+            this.lblBulletsFired.Location = new System.Drawing.Point(219, 258);
+            this.lblBulletsFired.Name = "lblBulletsFired";
+            this.lblBulletsFired.Size = new System.Drawing.Size(13, 13);
+            this.lblBulletsFired.TabIndex = 10;
+            this.lblBulletsFired.Text = "0";
+            // 
+            // lblAway
+            // 
+            this.lblAway.AutoSize = true;
+            this.lblAway.Location = new System.Drawing.Point(172, 258);
+            this.lblAway.Name = "lblAway";
+            this.lblAway.Size = new System.Drawing.Size(13, 13);
+            this.lblAway.TabIndex = 11;
+            this.lblAway.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(150, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Point Away";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::RussianRoulette.Resource1.Revolver;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(141, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(198, 162);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(98, 225);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Total";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(101, 259);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(13, 13);
+            this.lblTotal.TabIndex = 15;
+            this.lblTotal.Text = "0";
+            // 
+            // RussianRoulette
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 284);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblAway);
+            this.Controls.Add(this.lblBulletsFired);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblRandom);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSpin);
+            this.Controls.Add(this.btnPointAway);
+            this.Controls.Add(this.btnShoot);
+            this.Controls.Add(this.lblLosses);
+            this.Controls.Add(this.lblWins);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "RussianRoulette";
+            this.Text = "Russian Roulette";
+            this.Load += new System.EventHandler(this.RussianRoulette_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblWins;
+        private System.Windows.Forms.Label lblLosses;
+        private System.Windows.Forms.Button btnShoot;
+        private System.Windows.Forms.Button btnPointAway;
+        private System.Windows.Forms.Button btnSpin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblRandom;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblBulletsFired;
+        private System.Windows.Forms.Label lblAway;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ RussianRoulette/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+
+
+namespace RussianRoulette
+{
+    public partial class RussianRoulette : Form
+    {
+        public RussianRoulette()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnSpin_Click(object sender, EventArgs e)
+        {
+            Random random = new Random(DateTime.Now.Millisecond);
+            int ranNum;
+          
+            ranNum = random.Next(1, 7);
+            lblRandom.Text = ranNum.ToString();
+           
+            lblBulletsFired.Text = 0.ToString();
+            lblAway.Text = 0.ToString();
+
+            btnShoot.Enabled = true;
+            btnPointAway.Enabled = true;
+            btnSpin.Enabled = false;
+            
+        }
+
+        private void btnShoot_Click(object sender, EventArgs e)
+        {
+            bool kill = true;
+            calculate(kill);
+
+        }
+
+        private void btnPointAway_Click(object sender, EventArgs e)
+        {
+            
+            bool kill = false;
+            calculate(kill);
+        }
+
+        private void calculate(bool kill)
+        {
+            int ranNumber, bulletsFired, awayNumber, wins, losses, total;
+
+            wins = Convert.ToInt16(lblWins.Text);
+            losses = Convert.ToInt16(lblLosses.Text);
+            total = Convert.ToInt16(lblTotal.Text);
+            ranNumber = Convert.ToInt16(lblRandom.Text);
+            bulletsFired = Convert.ToInt16(lblBulletsFired.Text);
+            awayNumber = Convert.ToInt16(lblAway.Text);
+
+
+
+            bulletsFired++;
+
+             
+            if (bulletsFired == ranNumber || awayNumber == 2)
+            {
+                SoundPlayer audio = new SoundPlayer(global::RussianRoulette.Properties.Resources.Revolver);
+                audio.Play();
+                if (kill == true)
+            {
+                    
+                    MessageBox.Show("You are dead!");
+                    losses++;
+                }
+                else
+                {
+                    MessageBox.Show("You survived!");
+                    wins++;
+                }
+                total++;
+
+
+                btnShoot.Enabled = false;
+                btnPointAway.Enabled = false;
+                btnSpin.Enabled = true;
+
+
+            }
+            else
+            {
+                MessageBox.Show("Empty");
+            }
+            if (kill == false)
+            {
+                awayNumber++;
+            }
+
+            lblBulletsFired.Text = bulletsFired.ToString();
+            lblAway.Text = awayNumber.ToString();
+            if (awayNumber == 2)
+            {
+                btnPointAway.Enabled = false;
+            }
+            lblWins.Text = wins.ToString();
+            lblLosses.Text = losses.ToString();
+            lblTotal.Text = total.ToString();
+        }
+
+        private void RussianRoulette_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
